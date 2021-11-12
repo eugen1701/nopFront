8a8d164274aa0e786134f33809f3ce075d547f79
Robert Birliga <robertalexandru2712@gmail.com>
2021-11-13 01:19:46 +0200

    shorten

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import {
-  BrowserRouter as Router, Routes, Route, Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
 import { HomePage } from './pages/home-page/HomePage';
 import { AuthPage } from './pages/auth-page/AuthPage';

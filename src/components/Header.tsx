fb1475b88ba592de8fea42f7f207bfbdaea00489
Robert Birliga <robertalexandru2712@gmail.com>
2021-11-15 15:30:03 +0200

    Import logo svg. Set up some global styles. Code header html & css.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import "./Header.scss";
-
+import "../assets/shared_sass/_styles.scss";
+import nop_logo from '../assets/svgs/nop-logo.svg';
+import { Link } from 'react-router-dom';
 
 export const Header: React.FC = () => {
   return (
     <header id="header">
-      <h1>asdasd</h1>
+      <img src={nop_logo} id="header__nop-logo" alt="NOP Logo"/>
+
+      <nav id="header__nav" className="flex-row">
+        <ul id="header__nav-links" className="ul">
+          <li><Link to="/" className="link text-bold">Home</Link></li>
+          <li><Link to="/" className="link text-bold">About Us</Link></li>
+          <li><Link to="/" className="link text-bold">Contact</Link></li>
+        </ul>
+      </nav>
+
+      <ul id="header__nav-buttons" className="ul flex-pull-right">
+        <li><button className="button-green-neutral">Register</button></li>
+        <li><button className="button-no-background">Sign In</button></li>
+      </ul>
     </header>
   )
 }

5a21e8d68fdc3242acfbd22f93d48235d6a8e4b7
Robert Birliga <robertalexandru2712@gmail.com>
2021-11-16 18:26:57 +0200

    WIP Home Page Hero Section.

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import "./HomePage.scss";
+import home_page_hero_img from '../../assets/images/home-page-hero-img.png';
 
 export const HomePage: React.FC = () => {
   return (
     <div id="home-page">
       
+      <img id="home-page__hero-img" src={home_page_hero_img} alt=""/>
     </div>
   )
 }

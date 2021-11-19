7f6062a3dfff64171e6e059a7b836d57efaab78a
Alexandru Robert <robertalexandru2712@gmail.com>
2021-11-19 19:35:09 +0200

    WIP Home Page

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -8,7 +8,7 @@ export const HomePage: React.FC = () => {
     <div id="home-page" className="flex-row">
       <img id="home-page__hero-img" src={home_page_hero_img} alt=""/>
       <section id="home-page__hero-main-section">
-        <h1 className="h1 text-xlarge"> <span className="fancy-underline">SKYROCKET</span> YOUR DAILY FOOD DELIVERY BUSINESS RIGHT NOW</h1>
+        <h1 className="h1 text-xlarge"> <span className="fancy-underline">SKYROCKET</span> YOUR DAILY FOOD DELIVERY BUSINESS</h1>
         <br />
         <p className="text-medium">Partner with us to make a name for your local and reach thousands of customers across the country through our subscription-based platform, quickly & effortlessly.</p>
         <br /> <br />

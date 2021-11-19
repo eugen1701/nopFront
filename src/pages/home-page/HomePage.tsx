1cba3d0e67b4dfadf2adf119c392cc54cae8455a
Alexandru Robert <robertalexandru2712@gmail.com>
2021-11-19 19:27:55 +0200

    WIP Home Page

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import "./HomePage.scss";
+import "../..//assets/shared_sass/_styles.scss";
 import home_page_hero_img from '../../assets/images/home-page-hero-img.png';
 
 export const HomePage: React.FC = () => {
   return (
-    <div id="home-page">
-      
+    <div id="home-page" className="flex-row">
       <img id="home-page__hero-img" src={home_page_hero_img} alt=""/>
+      <section id="home-page__hero-main-section">
+        <h1 className="h1 text-xlarge"> <span className="fancy-underline">SKYROCKET</span> YOUR DAILY FOOD DELIVERY BUSINESS RIGHT NOW</h1>
+        <br />
+        <p className="text-medium">Partner with us to make a name for your local and reach thousands of customers across the country through our subscription-based platform, quickly & effortlessly.</p>
+        <br /> <br />
+        <button className="button-green button-xlarge">Register Your Kitchen</button>
+      </section>
     </div>
   )
 }

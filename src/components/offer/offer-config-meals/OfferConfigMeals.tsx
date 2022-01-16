39bd992f913822b6539635ca6f7c2f2c5caddc8e
eugen1701 <eugen.flocea@yahoo.com>
2022-01-16 16:22:52 +0200

    WIP

--- src/components/offer/offer-config-meals/OfferConfigMeals.tsx
+++ src/components/offer/offer-config-meals/OfferConfigMeals.tsx
@@ -1,11 +1,16 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import "./OfferConfigMeals.scss";
 import {IMeal, IIngredient} from "../../../models/meal/IMeal";
 import axios from "axios";
-import {Simulate} from "react-dom/test-utils";
-import error = Simulate.error;
 
-export const OfferConfigMeals: React.FC<{}> = () => {
+interface Props {
+  currentDay: number,
+  currentWeek: number,
+  meals: Array<IMeal>
+}
+
+
+export const OfferConfigMeals: React.FC<Props> = ({currentDay, currentWeek, meals}) => {
 
   const [mealInfo, setMealInfo] = useState<IMeal>({
     mealId: "",
@@ -29,84 +34,37 @@ export const OfferConfigMeals: React.FC<{}> = () => {
     unit: ""
   })
 
-  async function getIngredients(meal: IMeal) {
 
-    const token = localStorage.getItem("token");
-    const url = `http://localhost:7768/api/Meal/GetMeal/${meal.mealId}`;
-    const config = {
-      headers: {Authorization: `Bearer ${token}`}
-    };
-    try {
-      const response = await axios.get(url, config);
-      console.log("RESPONSE: ", response);
-      return response.data;
-    } catch (error: any) {
-      console.log(error);
-    }
-    return [];
-  }
 
-  async function getMeals(){
 
-    const token = localStorage.getItem("token");
-    const url =  "http://localhost:7768/api/Meal/Get";
-    const config = {
-      headers: {Authorization: `Bearer ${token}`}
-    };
-    try{
-      const response = await axios.get(url, config);
-      console.log("RESPONSE: ", response);
-      return response.data;
-    } catch (error: any) {
-      console.log(error);
-    }
-    return [];
-  }
 
-  async function renderIngredients(meal : IMeal) {
 
-    const ingredients = await getIngredients(meal);
-    console.log(ingredients);
-    let htmlIngredients = [];
-    for(let i of ingredients) {
-      htmlIngredients.push(
-          <button className="button button-green-neutral button-xsmall">{i.name}</button>
-      );
-    }
-  }
+  function renderMeals() {
+    return meals.map((meal: any) => (
+        <li>
+          <div>
+            <h4 className="text-medium">{meal.name}</h4>
+            <ul className="ul-ingredients">
+              <li>
+                ingredient
 
-  async function renderMeals() {
-    let htmlListOfMeals = [];
-    let meals = await getMeals();
-    for(let meal of meals) {
-       htmlListOfMeals.push(
-          <li>
-            <div>
-              <h4 className="text-medium">{meal.name}</h4>
-              <ul className="ul-ingredients">
-                <li>
-                  {renderIngredients(meal)}
+              </li>
+            </ul>
+            <br />
+            <p>{meal.description}</p>
+            <br />
+          </div>
 
-                </li>
-              </ul>
-              <br />
-              <p>{meal.description}</p>
-              <br />
-            </div>
-
-            <div>
-              <button className="button-no-background button-medium">Edit Meal</button>
-            </div>
-          </li>
-      );
-    }
-
-    return htmlListOfMeals;
+          <div>
+            <button className="button-no-background button-medium">Edit Meal</button>
+          </div>
+        </li>
+    ))
   }
   return (
     <section id="offer-config-meals">
       <h3 className="text-black text-xmedium">Meals</h3>
-      <br />  
+      <br />
       <ul id="offer-config-meals__ul" className="ul">
         {renderMeals()}
       </ul>
--- src/components/offer/offer-config/OfferConfig.tsx
+++ src/components/offer/offer-config/OfferConfig.tsx
@@ -1,19 +1,66 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import './OfferConfig.scss';
 import { IOffer } from '../../../models/offer/IOffer';
 import { OfferConfigRadioButtons } from '../offer-config-radio-buttons/OfferConfigRadioButtons';
 import { OfferConfigMeals } from "../offer-config-meals/OfferConfigMeals";
+import axios from "axios";
+import {IMeal} from "../../../models/meal/IMeal";
 
 export const OfferConfig: React.FC = () => {
-  const [offer, setOffer] = useState<IOffer>({ number_of_days: 21 });
-  const [currentWeek, setCurrentWeek] = useState<number>(-1);
-  const [currentDay, setCurrentDay] = useState<number>(-1);
-  const weekRadioButtonsTexts = initWeekRadioButtonsTexts();
-  const dayRadioButtonsTexts = initDayRadioButtonsTexts();
+    const [offer, setOffer] = useState<any>({numberOfDays: 7,
+    dayIds: []});
+    const [currentWeek, setCurrentWeek] = useState<number>(1);
+    const [currentDay, setCurrentDay] = useState<number>(1);
+    const weekRadioButtonsTexts = initWeekRadioButtonsTexts();
+    const dayRadioButtonsTexts = initDayRadioButtonsTexts();
 
+    async function getOffer() {
+        let offerID = document.URL.slice(document.URL.lastIndexOf("/") + 1);
+        console.log("The offer id is ", offerID);
+        const url = `http://localhost:7768/api/Offer/${offerID}`;
+        const token = localStorage.getItem("token");
+        const config = {
+            headers: {Authorization: `Bearer ${token}`}
+        }
+        try{
+            const response = await axios.get(url, config);
+            console.log("RESPONSE OFFER: ", response.data);
+            setOffer(response.data);
+            return response.data;
+        } catch (error: any) {
+            console.log(error);
+        }
+        return {};
+    }
+
+    const [meals, setMeals] = useState<Array<IMeal>>([])
+    const getMeals = async () => {
+        const offer1 = await getOffer();
+        const indexDay = (currentWeek-1) * 7 + currentDay-1;
+        console.log("current day"+ currentDay);
+        const token = localStorage.getItem("token");
+        console.log("offer: ", offer1);
+        console.log("indexDay: ", indexDay);
+        const url =  `http://localhost:7768/api/Meal/Day/${offer1.dayIds[indexDay]}`;
+
+        const config = {
+            headers: {Authorization: `Bearer ${token}`}
+        };
+        try{
+            const response = await axios.get(url, config);
+            console.log("RESPONSE day: ", indexDay, " response: ",  response);
+            setMeals(response.data);
+        } catch (error: any) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        console.log("hello useeffect");
+        getMeals();
+    }, []);
 
   function initWeekRadioButtonsTexts() {
-    const weeksCount = (offer.number_of_days ?? 0) / 7;
+    const weeksCount = (offer.numberOfDays ?? 0) / 7;
     const weekRadioButtonsTexts = [];
 
     for(let i = 1; i <= weeksCount; ++i) {
@@ -31,23 +78,26 @@ export const OfferConfig: React.FC = () => {
     <div id="offer-config">
       <h1 className="text-black text-large">Configure Standard Offer</h1>
       <br />
-    
+
       <h2>Week</h2>
-      <OfferConfigRadioButtons 
+      <OfferConfigRadioButtons
         options={ weekRadioButtonsTexts }
-        handleClick={ (i: number) => setCurrentWeek(i) }
+        handleClick={ (i: number) => {
+            setCurrentWeek(i);
+            getMeals().then();
+        } }
       />
       <br />
 
       <h2>Day</h2>
       <OfferConfigRadioButtons
         options={ dayRadioButtonsTexts }
-        handleClick={ (i: number) => setCurrentDay(i) }
+        handleClick={ (i: number) => {setCurrentDay(i); getMeals().then();} }
+
       />
       <br /><br />
 
-      <OfferConfigMeals />
+      <OfferConfigMeals currentDay={currentDay} currentWeek={currentWeek} meals={meals}/>
     </div>
   )
 }
-
